a612e180e0bd8fb406c77f262fb47105ffe2180e
Brian Izaki <bkizaki@outlook.com>
2021-10-02 15:21:47 -0300

    project personal config
    - add vue router

---
+++ src/components/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>Config inicial</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  created () {
+    document.title = "Home"
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,23 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import Home from './components/Home.vue'
 
 Vue.config.productionTip = false
 
+Vue.use(VueRouter)
+
+const routes = [
+  { path: '/', component: Home },
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  routes,
+  
+})
+
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
